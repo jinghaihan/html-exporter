faee39b020e6fefcfb6b766ff9fbba7e820bcf9b
jinghaihan <jinghaihan@ebupt.com>
2023-03-06 14:57:24 +0800

    feat(views): 实现路由自动注册

--- app/router.js
+++ app/router.js
@@ -1,8 +1,30 @@
 'use strict';
 
+const actions = {};
+
+function getAction(controller, path) {
+  const controllerArray = Object.keys(controller);
+  controllerArray.forEach(item => {
+    const newPath = path + '/' + item;
+    if (Object.keys(controller[item]).length > 0) {
+      getAction(controller[item], newPath);
+    } else {
+      actions[newPath] = controller[item];
+    }
+  });
+}
+
 
 module.exports = app => {
   const { router, controller } = app;
 
-  router.post('/api/pdf/export', controller.pdf.export);
+  const routePrefix = '/api';
+
+  getAction(controller, '');
+  const pathKeys = Object.keys(actions);
+
+  pathKeys.forEach(path => {
+    router.get(routePrefix + path, actions[path]);
+    router.post(routePrefix + path, actions[path]);
+  });
 };
